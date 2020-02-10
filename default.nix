b40baeaa74b8d1a44f03dcce7be3b35558898330
Marc Scott <marc@raspberrypi.org>
2020-02-10 09:09:38 +0000

    typo

--- default.nix
+++ default.nix
@@ -7,18 +7,10 @@ stdenv.mkDerivation {
 
 
   buildInputs = [
-    # these packages are required for virtualenv and pip to work:
-    #
     gcc6
     python3
-    python37Packages.virtualenv
-    python37Packages.matplotlib
-    # the following packages are related to the dependencies of your python
-    # project.
-    # In this particular example the python modules listed in the
-    # requirements.tx require the following packages to be installed locally
-    # in order to compile any binary extensions they may require.
-    #
+    virtualenv
+    matplotlib
     libgcc
     tkinter
     setuptools
@@ -33,6 +25,6 @@ stdenv.mkDerivation {
   virtualenv --no-setuptools venv
   export PATH=$PWD/venv/bin:$PATH
   pip install --no-cache-dir -r requirements.txt
-  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${gcc6.cc.lib}';
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${gcc6.cc.lib};
   '';
 }
